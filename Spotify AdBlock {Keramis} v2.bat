510bed916b46983d8a47fba1bcf12e2f1268f852
Keramis <81401952+Keramis@users.noreply.github.com>
2021-11-14 10:00:18 -0800

    Updated version.

---
+++ Spotify AdBlock {Keramis} v2.bat
@@ -0,0 +1,170 @@
+@echo off
+cd /
+cd Windows
+cd System32
+cd drivers
+cd etc
+echo  ________  ________  ________  _________  ___  ________ ___    ___ 
+echo ^|\   ____\^|\   __  \^|\   __  \^|\___   ___\\  \^|\  _____\\  \  /  /^|
+echo \ \  \___^|\ \  \^|\  \ \  \^|\  \^|___ \  \_\ \  \ \  \__/\ \  \/  / /
+echo  \ \_____  \ \   ____\ \  \\\  \   \ \  \ \ \  \ \   __\\ \    / / 
+echo   \^|____^|\  \ \  \___^|\ \  \\\  \   \ \  \ \ \  \ \  \_^| \/  /  /  
+echo     ____\_\  \ \__\    \ \_______\   \ \__\ \ \__\ \__\__/  / /    
+echo    ^|\_________\^|__^|     \^|_______^|    \^|__^|  \^|__^|\^|__^|\___/ /     
+echo    \^|_________^|                                       \^|___^|/      
+echo.
+echo.
+echo.
+echo  ________  ________  ________  ___       ________  ________  ___  __       
+echo ^|\   __  \^|\   ___ \^|\   __  \^|\  \     ^|\   __  \^|\   ____\^|\  \^|\  \     
+echo \ \  \^|\  \ \  \_^|\ \ \  \^|\ /\ \  \    \ \  \^|\  \ \  \___^|\ \  \/  /^|_   
+echo  \ \   __  \ \  \ \\ \ \   __  \ \  \    \ \  \\\  \ \  \    \ \   ___  \  
+echo   \ \  \ \  \ \  \_\\ \ \  \^|\  \ \  \____\ \  \\\  \ \  \____\ \  \\ \  \ 
+echo    \ \__\ \__\ \_______\ \_______\ \_______\ \_______\ \_______\ \__\\ \__\
+echo     \^|__^|\^|__^|\^|_______^|\^|_______^|\^|_______^|\^|_______^|\^|_______^|\^|__^| \^|__^|
+echo.
+echo.
+echo.
+echo  ___  ___  ________  ________  _________                           ________  ___       __      
+echo ^|\  \^|\  \^|\   __  \^|\   ____\^|\___   ___\                        ^|\   ____\^|\  \     ^|\  \    
+echo \ \  \\\  \ \  \^|\  \ \  \___^|\^|___ \  \_^|      ____________      \ \  \___^|\ \  \    \ \  \   
+echo  \ \   __  \ \  \\\  \ \_____  \   \ \  \      ^|\____________\     \ \  \    \ \  \  __\ \  \  
+echo   \ \  \ \  \ \  \\\  \^|____^|\  \   \ \  \     \^|____________^|      \ \  \____\ \  \^|\__\_\  \ 
+echo    \ \__\ \__\ \_______\____\_\  \   \ \__\                          \ \_______\ \____________\
+echo     \^|__^|\^|__^|\^|_______^|\_________\   \^|__^|                           \^|_______^|\^|____________^|
+echo                        \^|_________^|                                                            
+echo.
+echo.
+echo.
+timeout /t 3
+echo.
+echo.
+echo            ----- PLEASE MAKE SURE YOU RAN CMD AS **ADMINISTRATOR** OR THIS WILL NOT FUNCTION PROPERLY! -----
+echo            ----- PLEASE MAKE SURE YOU RAN CMD AS **ADMINISTRATOR** OR THIS WILL NOT FUNCTION PROPERLY! -----
+echo            ----- PLEASE MAKE SURE YOU RAN CMD AS **ADMINISTRATOR** OR THIS WILL NOT FUNCTION PROPERLY! -----
+echo            ----- PLEASE MAKE SURE YOU RAN CMD AS **ADMINISTRATOR** OR THIS WILL NOT FUNCTION PROPERLY! -----
+echo.
+echo.
+PAUSE
+echo.
+echo.
+echo            Once you have made sure, please continue.
+PAUSE
+timeout /t 1
+echo.
+echo.
+echo  ________  ________  ________   _________  ___  ________   ___  ___  ___  ________   ________                 
+echo ^|\   ____\^|\   __  \^|\   ___  \^|\___   ___\\  \^|\   ___  \^|\  \^|\  \^|\  \^|\   ___  \^|\   ____\                
+echo \ \  \___^|\ \  \^|\  \ \  \\ \  \^|___ \  \_\ \  \ \  \\ \  \ \  \\\  \ \  \ \  \\ \  \ \  \___^|                
+echo  \ \  \    \ \  \\\  \ \  \\ \  \   \ \  \ \ \  \ \  \\ \  \ \  \\\  \ \  \ \  \\ \  \ \  \  ___              
+echo   \ \  \____\ \  \\\  \ \  \\ \  \   \ \  \ \ \  \ \  \\ \  \ \  \\\  \ \  \ \  \\ \  \ \  \^|\  \ ___ ___ ___ 
+echo    \ \_______\ \_______\ \__\\ \__\   \ \__\ \ \__\ \__\\ \__\ \_______\ \__\ \__\\ \__\ \_______\\__\\__\\__\
+echo     \^|_______^|\^|_______^|\^|__^| \^|__^|    \^|__^|  \^|__^|\^|__^| \^|__^|\^|_______^|\^|__^|\^|__^| \^|__^|\^|_______\^|__\^|__\^|__^|
+timeout /t 2
+:: Continuing, inputting the ENTIRE hosts file here. This is gonna take a while to code...
+echo # Copyright (c) 1993-2009 Microsoft Corp. > hosts
+echo # >> hosts
+echo # This is a sample HOSTS file used by Microsoft TCP/IP for Windows. >> hosts
+echo # >> hosts
+echo # This file contains the mappings of IP addresses to host names. Each >> hosts
+echo # entry should be kept on an individual line. The IP address should >> hosts
+echo # be placed in the first column followed by the corresponding host name. >> hosts
+echo # The IP address and the host name should be separated by at least one >> hosts
+echo # space. >> hosts
+echo # >> hosts
+echo # Additionally, comments (such as these) may be inserted on individual >> hosts
+echo # lines or following the machine name denoted by a '#' symbol. >> hosts
+echo # >> hosts
+echo # For example: >> hosts
+echo # >> hosts
+echo #      102.54.94.97     rhino.acme.com          # source server >> hosts
+echo #       38.25.63.10     x.acme.com              # x client host >> hosts
+echo # >> hosts
+echo # localhost name resolution is handled within DNS itself. >> hosts
+echo #	127.0.0.1       localhost >> hosts
+echo #	::1             localhost >> hosts
+echo. >> hosts
+echo 0.0.0.0 adclick.g.doublecklick.net >> hosts
+echo. >> hosts
+echo 0.0.0.0 adeventtracker.spotify.com >> hosts
+echo. >> hosts
+echo 0.0.0.0 ads-fa.spotify.com >> hosts
+echo. >> hosts
+echo 0.0.0.0 analytics.spotify.com >> hosts
+echo. >> hosts
+echo 0.0.0.0 audio2.spotify.com >> hosts
+echo. >> hosts
+echo 0.0.0.0 b.scorecardresearch.com >> hosts
+echo. >> hosts
+echo 0.0.0.0 bounceexchange.com >> hosts
+echo. >> hosts
+echo 0.0.0.0 bs.serving-sys.com >> hosts
+echo. >> hosts
+echo 0.0.0.0 content.bitsontherun.com >> hosts
+echo. >> hosts
+echo 0.0.0.0 core.insightexpressai.com >> hosts
+echo. >> hosts
+echo 0.0.0.0 crashdump.spotify.com >> hosts
+echo. >> hosts
+echo 0.0.0.0 d2gi7ultltnc2u.cloudfront.net >> hosts
+echo. >> hosts
+echo 0.0.0.0 d3rt1990lpmkn.cloudfront.net >> hosts
+echo. >> hosts
+echo 0.0.0.0 desktop.spotify.com >> hosts
+echo. >> hosts
+echo 0.0.0.0 doubleclick.net >> hosts
+echo. >> hosts
+echo 0.0.0.0 ds.serving-sys.com >> hosts
+echo.  >> hosts
+echo 0.0.0.0 googleadservices.com >> hosts
+echo. >> hosts
+echo 0.0.0.0 googleads.g.doubleclick.net >> hosts
+echo. >> hosts
+echo 0.0.0.0 gtssl2-ocsp.geotrust.com >> hosts
+echo. >> hosts
+echo 0.0.0.0 js.moatads.com >> hosts
+echo.  >> hosts
+echo 0.0.0.0 log.spotify.com >> hosts
+echo. >> hosts
+echo 0.0.0.0 media-match.com >> hosts
+echo. >> hosts
+echo 0.0.0.0 omaze.com >> hosts
+echo.  >> hosts
+echo 0.0.0.0 open.spotify.com >> hosts
+echo. >> hosts
+echo 0.0.0.0 pagead46.l.doubleclick.net >> hosts
+echo. >> hosts
+echo 0.0.0.0 pagead2.googlesyndication.com >> hosts
+echo. >> hosts
+echo 0.0.0.0 partner.googleadservices.com >> hosts
+echo. >> hosts
+echo 0.0.0.0 pubads.g.doubleclick.net >> hosts
+echo. >> hosts
+echo 0.0.0.0 redirector.gvt1.com >> hosts
+echo. >> hosts
+echo 0.0.0.0 s0.2mdn.net >> hosts
+echo. >> hosts
+echo 0.0.0.0 securepubads.g.doubleclick.net >> hosts
+echo. >> hosts
+echo 0.0.0.0 spclient.wg.spotify.com >> hosts
+echo. >> hosts
+echo 0.0.0.0 tpc.googlesyndication.com >> hosts
+echo.  >> hosts
+echo 0.0.0.0 v.jwpcdn.com >> hosts
+echo.  >> hosts
+echo 0.0.0.0 video-ad-stats.googlesyndication.com >> hosts
+echo.  >> hosts
+echo 0.0.0.0 weblb-wg.gslb.spotify.com >> hosts
+echo. >> hosts
+echo 0.0.0.0 www.googleadservices.com >> hosts
+echo. >> hosts
+echo 0.0.0.0 www.omaze.com >> hosts
+timeout /t 3
+echo  ________  ________  _____ ______   ________  ___       _______  _________  _______   ________         
+echo ^|\   ____\^|\   __  \^|\   _ \  _   \^|\   __  \^|\  \     ^|\  ___ \^|\___   ___\\  ___ \ ^|\   ___ \        
+echo \ \  \___^|\ \  \^|\  \ \  \\\__\ \  \ \  \^|\  \ \  \    \ \   __/\^|___ \  \_\ \   __/^|\ \  \_^|\ \       
+echo  \ \  \    \ \  \\\  \ \  \\^|__^| \  \ \   ____\ \  \    \ \  \_^|/__  \ \  \ \ \  \_^|/_\ \  \ \\ \      
+echo   \ \  \____\ \  \\\  \ \  \    \ \  \ \  \___^|\ \  \____\ \  \_^|\ \  \ \  \ \ \  \_^|\ \ \  \_\\ \ ___ 
+echo    \ \_______\ \_______\ \__\    \ \__\ \__\    \ \_______\ \_______\  \ \__\ \ \_______\ \_______\\__\
+echo     \^|_______^|\^|_______^|\^|__^|     \^|__^|\^|__^|     \^|_______^|\^|_______^|   \^|__^|  \^|_______^|\^|_______\^|__^|
+PAUSE
